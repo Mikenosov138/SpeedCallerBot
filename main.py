4e4668208f348ac68c2e896c9de00be37f39c984
Mikenosov138 <mishanosovdog@gmail.com>
2026-04-08 21:39:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+print("=== 0. SHEBANG OK ===")
 import os
 print("1. OS OK")
 TOKEN = os.getenv("BOT_TOKEN")
@@ -6,4 +8,10 @@ import telebot
 print("3. Telebot OK")
 bot = telebot.TeleBot(TOKEN)
 print("4. BOT OK - ПИШИ /start!")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "✅ SpeedCallerBot РАБОТАЕТ!")
+
+print("5. HANDLER OK - START POLLING...")
 bot.infinity_polling()
