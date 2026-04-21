99e264356f8fd1d395638c498cad53ad91c0b0e4
Mikenosov138 <mishanosovdog@gmail.com>
2026-04-21 23:26:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -158,13 +158,14 @@ def send_current_number(chat_id, user_id):
     
     number_data, index, total = get_current_number(user_id)
     
-    if not number_data:
-        sent = bot.send_message(
-        chat_id, 
-        "📭 **Numbers finished!**\n✅ **Numbers loaded!** Press **🚀 START** to begin or **⬅️ BACK/SKIP** to page through...\n➕ Load new numbers"
+     if not number_data:
+    sent = bot.send_message(
+        chat_id,
+        "📭 **Numbers finished!** ✅ **Numbers loaded!** Press **🚀 START** to begin or **⬅️ BACK/SKIP** to page through... ➕ Load new numbers",
         reply_markup=main_menu_keyboard(),
         parse_mode='Markdown'
     )
+
     else:
         num_id, phone = number_data
         
